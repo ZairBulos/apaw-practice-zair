c3547536cf0141d91140679a662f492072962d43
Zair Bulos <buloszair@gmail.com>
2025-04-07 15:47:14 -0300

    feat: Add persistence layer. #2

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/museum/persistence/ArtworkPersistenceMongodb.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.museum.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.museum.ArtworkPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("artworkPersistence")
+public class ArtworkPersistenceMongodb implements ArtworkPersistence {
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/museum/persistence/MuseumPersistenceMongodb.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.museum.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.museum.MuseumPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("museumPersistence")
+public class MuseumPersistenceMongodb implements MuseumPersistence {
+}
